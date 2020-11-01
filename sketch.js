41b4288e416f872038af4f24dcd2d953d9d1efc6
khushisahni <73423544+khushisahni@users.noreply.github.com>
2020-11-01 16:20:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,165 @@
+
+var monkey , monkey_running, monkeyCollide;
+var  invisiGround;
+var banana ,bananaImage, obstacle, obstacleImage;
+var FoodGroup, obstacleGroup;
+var score = 0;
+var bananaScore = 0;
+var PLAY = 0;
+var END = 1;
+var gameState = PLAY;
+
+function preload(){
+  monkey_running = loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  monkeyCollide = loadAnimation("sprite_1.png");
+  
+  
+  
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+ 
+}
+
+function setup(){
+ createCanvas(600,300);
+  
+  obstacleGroup = createGroup();
+  bananaGroup = createGroup();
+  
+ 
+  monkey = createSprite(80,230,10,10);
+  monkey.scale = 0.12;
+  monkey.addAnimation("monkey", monkey_running);
+  monkey.addAnimation("collide", monkeyCollide);
+   
+   
+  
+  invisiGround = createSprite(300,278,600,7);
+  invisiGround.visible = false;
+  
+}
+
+function draw(){
+  background("skyblue");
+  fill("black");
+  text("SURVIVAL TIME: "+score, 470, 20);
+  text("BANANAS COLLECTED: "+bananaScore,300,20);
+  
+  //make it reset
+  if(invisiGround.x < 0){
+     ground.x = ground.width/2
+     
+     }
+  
+  if (gameState === PLAY){
+    obstacles();
+    bananas();
+    score = score + Math.round(getFrameRate()/60);
+    
+    
+  
+    if(keyDown("space")&&monkey.y >= 235) {
+      monkey.velocityY = -13; 
+    }
+    if(keyDown("right")){
+       monkey.x = monkey.x + 3;
+       
+       }
+    if(keyDown("left")){
+       monkey.x = monkey.x - 3;
+       
+       }
+  
+    monkey.velocityY = monkey.velocityY + 0.8
+  
+    
+    for(var i = 0; i < bananaGroup.length; i++ ){
+    if (monkey.isTouching(bananaGroup.get(i))){
+      bananaScore++;  
+      bananaGroup.get(i).destroy();
+    
+    }
+    }
+    if (obstacleGroup.isTouching(monkey)){
+      gameState = END;
+    }
+    
+  }
+  
+  if (gameState === END){
+   
+    
+    monkey.y = 235;
+    monkey.scale = 0.12;
+    monkey.changeAnimation("collide", monkeyCollide);
+    
+    obstacleGroup.setVelocityXEach(0);
+    bananaGroup.setVelocityXEach(0);
+    obstacleGroup.setLifetimeEach(-1);
+    bananaGroup.setLifetimeEach(-1);
+    fill("red")
+    stroke("black")
+    textSize(30);
+    text("GAMEOVER!!!", 220, 170);
+    fill("black");
+    textSize(15);
+    text("Press 'R' to play again", 240, 200);
+    
+    if (keyDown("r")){
+      bananaGroup.destroyEach();
+      obstacleGroup.destroyEach();
+      monkey.changeAnimation("monkey", monkey_running);
+      score = 0;
+      bananaScore = 0;
+      gameState = PLAY; 
+    }
+  }
+  
+  
+  
+  drawSprites(); 
+  
+  monkey.collide(invisiGround);
+}
+
+function bananas(){
+  if (frameCount%80 === 0){
+    
+    banana = createSprite(620,120, 50, 50 )
+    banana.addAnimation("banana", bananaImage);
+    banana.scale = 0.1;
+    banana.velocityX =-(4+score*1.5/100);           
+    banana.lifetime = 220;
+    bananaGroup.add(banana);
+    bananaGroup.add(banana);
+
+    
+  }
+  
+
+  
+}
+
+function obstacles(){
+  if (frameCount%200 === 0){
+    
+    obstacle = createSprite(620,253,50,50);
+    obstacle.addAnimation("rock", obstacleImage);
+    obstacle.setCollider("circle", 0, 0, 180);
+    obstacle.scale = 0.13 ;
+    obstacle.velocityX = -(4+score*1.5/100);
+    obstacle.lifetime = 220;
+    obstacleGroup.add(obstacle);
+    
+  }
+  
+  
+}
+
+
+
+
+
+
